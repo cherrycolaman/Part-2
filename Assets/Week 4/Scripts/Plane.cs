bae3ffde4d9b4602b3b371101c7ce4a19ca9ecb4
loet <loet@OA-S141-21>
2024-02-01 10:50:58 -0500

    added gameObject destruction

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -11,6 +11,8 @@ public class Plane : MonoBehaviour
     Vector2 currentPosition;
     Rigidbody2D rigidbody;
     public float speed = 1;
+    public AnimationCurve landing;
+    float landingTimer;
 
     private void Start()
     {
@@ -39,6 +41,16 @@ public class Plane : MonoBehaviour
     }
     private void Update()
     {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            landingTimer += 0.5f * Time.deltaTime;
+            float interpolation = landing.Evaluate(landingTimer);
+            if (transform.localScale.z < 0.1f)
+            {
+                Destroy(gameObject);
+            }
+            transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, interpolation);
+        }
         lineRenderer.SetPosition(0, transform.position);
         if (points.Count > 0 )
         {
