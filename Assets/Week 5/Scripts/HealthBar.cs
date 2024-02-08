8e32efc5c18fc9753568a3ad3d93ec6af0038698
loet <loet@OA-S141-21>
2024-02-08 11:19:55 -0500

    made the health bar uninteractable and added health bar texture

---
+++ Assets/Week 5/Scripts/HealthBar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngineInternal;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TakeDamage()
+    {
+
+    }
+}
