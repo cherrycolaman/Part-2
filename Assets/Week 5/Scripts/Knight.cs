df38f4b2b6065f2a32c01c1ba58a0ade59f5cb93
loet <loet@OA-S141-21>
2024-02-08 10:44:34 -0500

    added assets, and knight animations

---
+++ Assets/Week 5/Scripts/Knight.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+    Vector2 destination;
+    Vector2 movement;
+    public float speed = 3;
+    Rigidbody2D rb;
+    Animator animator;
+    bool clickingOnSelf = false;
+    public float health;
+    public float maxHealth = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        health = maxHealth;
+    }
+    private void FixedUpdate()
+    {
+        movement = destination - (Vector2)transform.position;
+        if(movement.magnitude < 0.1)
+        {
+            movement = Vector2.zero;
+        }
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !clickingOnSelf)
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        animator.SetFloat("Movement", movement.magnitude);
+    }
+    private void OnMouseDown()
+    {
+        clickingOnSelf = true;
+        TakeDamage(1);
+    }
+    private void OnMouseUp()
+    {
+        clickingOnSelf = false;
+    }
+    void TakeDamage(float damage)
+    {
+        health -= damage;
+        health = Mathf.Clamp(health, 0, maxHealth);
+        if (health == 0)
+        {
+            animator.SetTrigger("Death");
+        }
+        else
+        {
+            animator.SetTrigger("TakeDamage");
+        }
+    }
+}
