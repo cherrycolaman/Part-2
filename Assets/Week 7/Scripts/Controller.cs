c49e92b45ad560e45aaf8c44b3fffafca287a7f6
loet <loet@OA-S141-21>
2024-02-22 11:19:14 -0500

    finished player movement

--- Assets/Week 7/Scripts/Controller.cs
+++ Assets/Week 7/Scripts/Controller.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public Slider chargeSlider;
+    float chargeValue;
+    public float maxCharge = 1;
+    Vector2 direction;
     public static FootballPlayer SelectedPlayer { get; private set; }
     public static void SetSelectedPlayer(FootballPlayer player)
     {
@@ -21,10 +25,33 @@ public class Controller : MonoBehaviour
     {
         
     }
-
+    private void FixedUpdate()
+    {
+        if(direction != Vector2.zero)
+        {
+            SelectedPlayer.Move(direction);
+            direction = Vector2.zero;
+            chargeValue = 0;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (SelectedPlayer == null) return;
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            chargeValue = 0;
+            direction = Vector2.zero;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            chargeValue += Time.deltaTime;
+            chargeValue = Mathf.Clamp(chargeValue, 0, maxCharge);
+            chargeSlider.value = chargeValue;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            direction = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)SelectedPlayer.transform.position).normalized * chargeValue;
+        }
     }
 }
--- Assets/Week 7/Scripts/FootballPlayer.cs
+++ Assets/Week 7/Scripts/FootballPlayer.cs
@@ -9,10 +9,13 @@ public class FootballPlayer : MonoBehaviour
     SpriteRenderer spriteRenderer;
     public Color selectedColor;
     public Color unselectedColor;
+    Rigidbody2D rb;
+    public float speed = 1000;
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
         Selected(false);
     }
     private void OnMouseDown()
@@ -34,5 +37,9 @@ public class FootballPlayer : MonoBehaviour
         {
             spriteRenderer.color = unselectedColor;
         }
+    }
+    public void Move(Vector2 direction)
+    {
+        rb.AddForce(direction * speed);
     }
  }
