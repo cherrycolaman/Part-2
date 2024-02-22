d6b89091554c4717e6020d803d12b9b5d3781f76
DESKTOP-5A2LTEH\GamerTech <loet@sheridancollege.ca>
2024-02-21 21:08:58 -0500

    added player movement

--- Assets/Assignment/Scripts/PixelGuy.cs
+++ Assets/Assignment/Scripts/PixelGuy.cs
@@ -21,10 +21,22 @@ public class PixelGuy : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
-
+    private void FixedUpdate()
+    {
+        movement = destination - (Vector2)transform.position;
+        if (movement.magnitude < 0.1)
+        {
+            movement = Vector2.zero;
+        }
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        animator.SetFloat("Movement", movement.magnitude);
     }
 }
