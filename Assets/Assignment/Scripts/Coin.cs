3b33378adb7b2be1c4c71ba9987d4c160ecd704b
DESKTOP-5A2LTEH\GamerTech <loet@sheridancollege.ca>
2024-02-25 16:05:23 -0500

    made it so coin and dagger cannot collide, added coin location randomization (unfinished)

---
+++ Assets/Assignment/Scripts/Coin.cs
@@ -0,0 +1,31 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Coin : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public int score;
+    Vector2 coinPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        score += 1;
+        coinPosition.x = UnityEngine.Random.Range(-6.9f, 3.1f);
+        coinPosition.y = UnityEngine.Random.Range(-3.9f, 3.9f);
+        rb.MovePosition(coinPosition);
+    }
+}
