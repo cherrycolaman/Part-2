6f3dcd46e3eb99732753bd3fe4ac82957cb2dd35
DESKTOP-5A2LTEH\GamerTech <loet@sheridancollege.ca>
2024-02-26 11:44:51 -0500

    pressing retry loads back previous scene

---
+++ Assets/Assignment/Scripts/Reload.cs
@@ -0,0 +1,27 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reload : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ReloadScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex - 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
